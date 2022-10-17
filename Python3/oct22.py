144986136796a9a90c298c737c4ce80f91816bf8
fde9 <kyaruny@gmail.com>
2022-10-18 02:32:22 +0800

    难

---
+++ Python3/oct22.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def atMostNGivenDigitSet(self, digits: List[str], n: int) -> int:
+        '''
+            902 最大为 N 的数字组合
+            https://leetcode.cn/problems/numbers-at-most-n-given-digit-set/
+        '''
+        N = len(digits)
+        s = str(n)
+        L = len(s)
+        dp = [[0,0] for _ in range(L+1)]
+        dp[0][1] = 1
+        for i in range(1, L + 1):
+            for d in digits:
+                if d == s[i - 1]:
+                    dp[i][1] = dp[i-1][1]
+                elif d < s[i - 1]:
+                    dp[i][0] += dp[i-1][1]
+                else:
+                    break
+            if i > 1:
+                dp[i][0] += dp[i - 1][0] * N + N
+
+        return sum(dp[L])

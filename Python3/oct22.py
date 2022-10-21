51c50eeb24915402389e9385c78c9286dfeb862b
fde9 <kyaruny@gmail.com>
2022-10-22 00:38:45 +0800

    format

--- Python3/oct22.py
+++ Python3/oct22.py
@@ -2,7 +2,9 @@ from collections import Counter
 from math import inf
 from typing import List
 
+
 class Solution:
+
     def atMostNGivenDigitSet(self, digits: List[str], n: int) -> int:
         '''
             # 902. 最大为 N 的数字组合
@@ -11,14 +13,14 @@ class Solution:
         N = len(digits)
         s = str(n)
         L = len(s)
-        dp = [[0,0] for _ in range(L+1)]
+        dp = [[0, 0] for _ in range(L + 1)]
         dp[0][1] = 1
         for i in range(1, L + 1):
             for d in digits:
                 if d == s[i - 1]:
-                    dp[i][1] = dp[i-1][1]
+                    dp[i][1] = dp[i - 1][1]
                 elif d < s[i - 1]:
-                    dp[i][0] += dp[i-1][1]
+                    dp[i][0] += dp[i - 1][1]
                 else:
                     break
             if i > 1:
@@ -34,7 +36,7 @@ class Solution:
         count = Counter(students)
         for i in sandwiches:
             if count[i] == 0:
-                return count[1-i]
+                return count[1 - i]
             count[i] -= 1
         return 0
 
@@ -45,12 +47,24 @@ class Solution:
         '''
         if k == 1:
             return 0
-        if k > (1 << (n-2)):
-            return 1 ^ self.kthGrammar(n-1, k - (1 << (n-2)))
-        return self.kthGrammar(n-1, k)
+        if k > (1 << (n - 2)):
+            return 1 ^ self.kthGrammar(n - 1, k - (1 << (n - 2)))
+        return self.kthGrammar(n - 1, k)
+
+    def jobScheduling(self, startTime: List[int], endTime: List[int],
+                      profit: List[int]) -> int:
+        '''
+            # 1235. 规划兼职工作
+            https://leetcode.cn/problems/maximum-profit-in-job-scheduling/
+        '''
+        pass
 
 
 class StockSpanner:
+    '''
+        # 901. 股票价格跨度
+        https://leetcode.cn/problems/online-stock-span/
+    '''
 
     def __init__(self):
         self.stk = [(-1, inf)]
